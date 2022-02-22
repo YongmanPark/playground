f5583c3729b8c56a5d67ee107422e05ec4f97a46
yongman park <yongman514@gmail.com>
2022-02-22 17:10:16 +0900

    about state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
 
   let posts = 'test contents';
   let styles = {color: 'gold', fontSize: '30px'}
 
+  let [글제목,글제목변경] = useState(['남자 코트 추천','우동 맛집 추천', '일단 추천 한다']);
+  let [따봉, 따봉변경] = useState(0);
+
+  function 제목변경(){
+    var newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    newArray[1] = '남자 코트 추천';
+    newArray[2] = '애기 코트 추천';
+
+    글제목변경(newArray);
+  }
+
   return (
     <div className="App">
       <div className='black-nav'>
         <div style={styles }>Blog Title </div>
       </div>
-      <h4>{posts}</h4>
+
+   
+      <button onClick={ 제목변경 }>change</button>
+      <hr/>
+      <div className='list'>
+        <h3>{글제목[0]} <span onClick={ () => {}}>👍</span>{따봉}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3>{글제목[1]}</h3>
+        <p>2월 17일 발행</p> 
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3>{글제목[2]}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+
     </div>
   );
 }
